1a326d4053c7329058a43ea391a23a9f2d1c975b
kingcons <brit@kingcons.io>
2019-02-05 11:17:54 -0500

    Intro to ORMs.

--- 08-intro-to-orms/lib/tweet.rb
+++ 08-intro-to-orms/lib/tweet.rb
@@ -1,14 +1,21 @@
 class Tweet
   attr_accessor :message, :username
-  ALL = []
+  attr_reader :id
 
   def self.all
-    ALL
+    DB[:conn].execute("SELECT * FROM tweets").map { |t| Tweet.new(t) }
   end
 
   def initialize(props={})
+    @id = props['id']
     @message = props['message']
     @username = props['username']
-    ALL << self
+    self.save
+  end
+
+  def save
+    unless @id
+      DB[:conn].execute("INSERT INTO tweets (username, message) VALUES ('#{username}', '#{message}')")
+    end
   end
 end
