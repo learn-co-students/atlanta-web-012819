a101529672e6e0c369c04e6e3ed3939f2aea6f1e
Emily Wilder <emilywilder.earth@gmail.com>
2019-02-21 11:34:49 -0500

    after lecture
    finished rest routes
    added author model
    fixed most of the views

--- 13-sinatra-forms-associated-objects/app/controllers/application_controller.rb
+++ 13-sinatra-forms-associated-objects/app/controllers/application_controller.rb
@@ -1,37 +1,63 @@
 class ApplicationController < Sinatra::Base
   configure do
     # set :views, "app/views"
+    set :method_override, true
     set :root, "app"
   end
+
+  ## App
 
   get '/' do
     'this is working'
   end
 
-  # index route for books
+  ## Books
+
+  # index
   get '/books' do
     # render an index erb file
     # index erb should have: list of the books
-    @books = Book.all.limit(20)
+    @books = Book.all
     erb :index
   end
 
-  # new route for books
+  # new
   get '/books/new' do
     erb :new
   end
 
-  # create route for books
-  post '/books' do
-    book = Book.create(params)
-    redirect "/books/#{book.id}"
-  end
-
-  # show route for books
+  # show
   get '/books/:id' do
     @book = Book.find(params[:id])
     erb :show
   end
 
+  # create
+  post '/books' do
+    book = Book.create(params)
+    redirect "/books/#{book.id}"
+  end
 
+  # edit
+  get '/books/:id/edit' do
+    @book = Book.find(params[:id])
+    erb :edit
+  end
+
+  # update
+  patch '/books/:id' do
+    @book = Book.find(params[:id])
+    params.delete("_method")
+    if @book.update(params)
+      redirect "/books/#{@book.id}"
+    else
+      erb :edit
+    end
+  end
+
+  # delete
+  delete '/books/:id' do
+    Book.destroy(params[:id])
+    redirect '/books'
+  end
 end
